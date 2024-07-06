b5b8d6830a2e2349f7bf9e42685bf27896409909
KamiUnitY <kamiunity141@gmail.com>
2024-07-07 05:38:04 +0700

    Removed spaces

--- guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation.lua
+++ guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation.lua
@@ -45,20 +45,20 @@ local _input_hook = function(func, self, action_name)
     local out = func(self, action_name)
     local type_str = type(out)
     if (type_str == "boolean" and out == true) or (type_str == "number" and out == 1) then
-        if 
+        if
             action_name == "combat_ability_pressed"
             or action_name == "combat_ability_hold"
             or action_name == "combat_ability_release"
         then
             mod.debug.print(action_name)
         end
-        local num_charges = getCombatAbilityNumCharges()    
-        if 
+        local num_charges = getCombatAbilityNumCharges()
+        if
             action_name == "combat_ability_pressed" and num_charges ~= nil and num_charges > 0
         then
             setPromise()
         end
-        if 
+        if
             action_name == "combat_ability_hold" and not mod:get("enable_combat_ability_hold")
         then
             return false
@@ -83,13 +83,11 @@ end)
 
 
 mod:hook_require("scripts/extension_systems/weapon/actions/action_base", function(ActionAbilityBase)
-    mod:hook_safe(ActionAbilityBase, "start", function (self, action_settings, t, time_scale, action_start_params)
+    mod:hook_safe(ActionAbilityBase, "start", function(self, action_settings, t, time_scale, action_start_params)
         if action_settings.ability_type == "combat_ability" then
             mod.debug.print("ActionAbilityBase: Start")
             mod.debug.print(action_settings)
             mod.promise_ability = false
         end
-        
-
     end)
 end)
