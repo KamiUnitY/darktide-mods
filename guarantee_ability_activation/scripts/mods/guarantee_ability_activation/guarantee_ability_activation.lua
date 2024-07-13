50ab9431a5ab3898408468e4a920aa1cd65e8913
KamiUnitY <kamiunity141@gmail.com>
2024-07-13 16:31:50 +0700

    Update version

--- guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation.lua
+++ guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation.lua
@@ -1,4 +1,4 @@
--- Guarantee Ability Activation mod by KamiUnitY. Ver. 1.1.4
+-- Guarantee Ability Activation mod by KamiUnitY. Ver. 1.1.5
 
 local mod = get_mod("guarantee_ability_activation")
 local modding_tools = get_mod("modding_tools")
