48e8ad4b90fcbda7d8100992a872f514135ed126
deluxghost <deluxghost@users.noreply.github.com>
2024-07-17 15:32:42 +0800

    add zh-cn translations

--- guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation_localization.lua
+++ guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation_localization.lua
@@ -1,35 +1,46 @@
 return {
 	mod_name = {
-		en = "Guarantee Ability Activation"
+		en = "Guarantee Ability Activation",
+		["zh-cn"] = "保证技能激活",
 	},
 	mod_description = {
 		en = "Guarantee combat ability activation when player try to activate them in middle of animation",
+		["zh-cn"] = "确保玩家在动画中途能成功激活主动技能",
 	},
 	enable_prevent_cancel_on_short_ability_press = {
 		en = "Prevent cancel on short ability press",
+		["zh-cn"] = "防止短按时取消激活",
 	},
 	enable_prevent_cancel_on_short_ability_press_description = {
 		en = "Basically preventing ability cancel on freshly hold ability",
+		["zh-cn"] = "防止需要按住的技能被短按取消",
 	},
 	enable_prevent_cancel_on_start_sprinting = {
 		en = "Prevent cancel on start sprinting",
+		["zh-cn"] = "防止开始冲刺时取消激活",
 	},
 	enable_prevent_cancel_on_start_sprinting_description = {
 		en = "Promise ability key press when start sprinting while holding ability key",
+		["zh-cn"] = "如果在按住技能键的情况下开始冲刺，则保证技能键被触发",
 	},
 	enable_prevent_double_dashing = {
 		en = "Prevent double dashing",
+		["zh-cn"] = "防止二次冲锋",
 	},
 	enable_prevent_double_dashing_description = {
 		en = "Blocking ability key press while dashing. Shouldn't be necessary to enable this if you could change the habit of key spamming.",
+		["zh-cn"] = "在冲锋过程中阻止技能键。如果你能改掉连续按技能键的习惯，则不需要启用。",
 	},
 	enable_prevent_ability_aiming = {
 		en = "Disable ability holding feature",
+		["zh-cn"] = "禁用技能键按住功能",
 	},
 	debug_group = {
 		en = "Debug",
+		["zh-cn"] = "调试",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
+		["zh-cn"] = "模组开发者工具",
 	},
 }
--- guarantee_weapon_swap/scripts/mods/guarantee_weapon_swap/guarantee_weapon_swap_localization.lua
+++ guarantee_weapon_swap/scripts/mods/guarantee_weapon_swap/guarantee_weapon_swap_localization.lua
@@ -1,17 +1,22 @@
 return {
 	mod_name = {
-		en = "Guarantee Weapon Swap"
+		en = "Guarantee Weapon Swap",
+		["zh-cn"] = "保证武器切换",
 	},
 	mod_description = {
 		en = "Guarantee weapon swapping when player try to swap weapon in middle of animation",
+		["zh-cn"] = "确保玩家在动画中途能成功切换武器",
 	},
 	enable_zealot_throwing_knives = {
 		en = "Zealot's Blades of Faith",
+		["zh-cn"] = "狂信徒的信仰之刃",
 	},
 	debug_group = {
 		en = "Debug",
+		["zh-cn"] = "调试",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
+		["zh-cn"] = "模组开发者工具",
 	},
 }
