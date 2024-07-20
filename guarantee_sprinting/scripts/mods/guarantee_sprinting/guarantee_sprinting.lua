3c46f4fd9bb91813a96ea4640be92aa94597c70c
KamiUnitY <kamiunity141@gmail.com>
2024-07-20 20:03:54 +0700

    Changed requirement from need Hold Sprint OFF to ON

--- guarantee_sprinting/scripts/mods/guarantee_sprinting/guarantee_sprinting.lua
+++ guarantee_sprinting/scripts/mods/guarantee_sprinting/guarantee_sprinting.lua
@@ -4,8 +4,6 @@ local Sprint = require("scripts/extension_systems/character_state_machine/charac
 
 mod.promise_sprint = false
 mod.pressed_forward = false
-mod.interrupt_sprint = false
-local last_pressed_interrupt = os.clock()
 
 local debug = {
     is_enabled = function(self)
@@ -17,15 +15,8 @@ local debug = {
 }
 
 mod.on_all_mods_loaded = function()
-    modding_tools:watch("pressed_forward", mod, "pressed_forward")
-    modding_tools:watch("interrupt_sprint", mod, "interrupt_sprint")
+    -- modding_tools:watch("pressed_forward", mod, "pressed_forward")
 end
-
-local elapsed = function (time)
-    return os.clock() - time
-end
-
-local INTURRUPT_DELAY = 0.3
 
 local ALLOWED_CHARACTER_STATE = {
     dodging = true,
@@ -71,7 +62,7 @@ local function clearPromise(from)
 end
 
 local function isPromised()
-    local result = mod.promise_sprint and mod.pressed_forward and mod.character_state == "walking" and not mod.interrupt_sprint
+    local result = mod.promise_sprint and mod.pressed_forward
     if result then
         if debug:is_enabled() then
             debug:print("Guarantee Sprinting: Attempting to sprint for you !!!")
@@ -94,26 +85,14 @@ local _input_hook = function(func, self, action_name)
     end
 
     if action_name == "sprint" and pressed then
-        if mod.character_state == "sprinting" then
-            return false
-        else
-            setPromise("Pressed Sprint");
-        end
-        return out
-    end
-
-    if action_name == "sprinting" and pressed then
-        if not mod.interrupt_sprint and elapsed(last_pressed_interrupt) > INTURRUPT_DELAY then
-            setPromise("Hold Sprinting");
-        end
+        setPromise("Pressed Sprint");
     end
 
     if INTERRUPTED_INPUT[action_name] and pressed then
         clearPromise("Pressed INTERRUPTED_INPUT: " .. action_name)
-        last_pressed_interrupt = os.clock()
     end
 
-    if action_name == "sprint" then
+    if action_name == "sprinting" then
         return out or isPromised()
     end
 
@@ -130,29 +109,4 @@ mod:hook_safe("CharacterStateMachine", "fixed_update", function(self, unit, dt, 
             mod.promise_sprint = false
         end
     end
-end)
-
-mod:hook_require("scripts/extension_systems/character_state_machine/character_states/utilities/sprint", function(instance)
-    mod:hook_safe(instance, "sprint_input", function(input_source, is_sprinting, sprint_requires_press_to_interrupt)
-        mod.interrupt_sprint = not not sprint_requires_press_to_interrupt
-    end)
-
-    -- WORKS IN PSYKHANIUM BUT NOT IN MISSION
-    -- mod:hook(instance, "sprint_input", function(func, input_source, is_sprinting, sprint_requires_press_to_interrupt)
-    --     local wants_sprint = is_sprinting
-    --     local hold_to_sprint = input_source:get("hold_to_sprint")
-
-    --     if hold_to_sprint then
-    --         if is_sprinting or not sprint_requires_press_to_interrupt then
-    --             wants_sprint = input_source:get("sprinting")
-    --         end
-    --     else
-    --         if input_source:get("sprint") then
-    --             wants_sprint = true
-    --         elseif is_sprinting or not sprint_requires_press_to_interrupt then
-    --             wants_sprint = mod.promise_sprint or input_source:get("sprinting")
-    --         end
-    --     end
-    --     return wants_sprint
-    -- end)
 end)
