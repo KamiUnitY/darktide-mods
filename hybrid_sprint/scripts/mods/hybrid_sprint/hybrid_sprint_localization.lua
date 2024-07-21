2354026cdf5842065ab2edea9ae3b956f5a2b84e
deluxghost <deluxghost@users.noreply.github.com>
2024-07-21 12:15:24 +0800

    Update hybrid_sprint_localization.lua

--- hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint_localization.lua
+++ hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint_localization.lua
@@ -1,20 +1,26 @@
 return {
 	mod_name = {
 		en = "Hybrid Sprint",
+		["zh-cn"] = "混合式疾跑",
 	},
 	mod_description = {
 		en = "Provide better sprinting experience by combining Toggle and Hold sprinting mechanic together. Sprint can only be canceled by releasing forward key and weapon actions.",
+		["zh-cn"] = "结合切换疾跑和长按疾跑，提供体验更好的混合式疾跑。只有松开前进键和武器操作可以打断疾跑。",
 	},
 	enable_hold_to_sprint = {
 		en = "Hold to Sprint",
+		["zh-cn"] = "长按疾跑",
 	},
 	enable_hold_to_sprint_description = {
 		en = "Only Sprint while holding the button, Basically disable this mod.",
+		["zh-cn"] = "仅在长按时疾跑，相当于禁用此模组。",
 	},
 	debug_group = {
 		en = "Debug",
+		["zh-cn"] = "调试",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
+		["zh-cn"] = "模组开发者工具",
 	},
 }
