8ca2e8d3210ba10fccdf1b78cdd8ec7014a29447
KamiUnitY <kamiunity141@gmail.com>
2024-07-22 23:02:20 +0700

    Update version

--- hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint.lua
+++ hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint.lua
@@ -1,4 +1,4 @@
--- Guarantee Better Sprint mod by KamiUnitY. Ver. 1.0.4
+-- Guarantee Better Sprint mod by KamiUnitY. Ver. 1.0.5
 
 local mod = get_mod("hybrid_sprint")
 local modding_tools = get_mod("modding_tools")
