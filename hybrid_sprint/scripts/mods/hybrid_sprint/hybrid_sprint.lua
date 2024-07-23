f6207f2197d974fd41d6f61dd8e358f175207697
KamiUnitY <kamiunity141@gmail.com>
2024-07-23 22:21:11 +0700

    Add settings cache

--- hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint.lua
+++ hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint.lua
@@ -3,6 +3,36 @@
 local mod = get_mod("hybrid_sprint")
 local modding_tools = get_mod("modding_tools")
 
+mod.settings = {
+    enable_hold_to_sprint                   = mod:get("enable_hold_to_sprint"),
+    enable_keep_sprint_after_weapon_actions = mod:get("enable_keep_sprint_after_weapon_actions"),
+    enable_debug_modding_tools              = mod:get("enable_debug_modding_tools"),
+}
+
+mod.on_setting_changed = function(setting_id)
+    mod.settings[setting_id] = mod:get(setting_id)
+end
+
+mod.on_all_mods_loaded = function()
+    -- modding_tools:watch("pressed_forward", mod, "pressed_forward")
+    -- modding_tools:watch("interrupt_sprint", mod, "interrupt_sprint")
+    -- modding_tools:watch("character_state", mod, "character_state")
+end
+
+local debug = {
+    is_enabled = function(self)
+        return modding_tools and modding_tools:is_enabled() and mod.settings["enable_debug_modding_tools"]
+    end,
+    print = function(self, text)
+        pcall(function() modding_tools:console_print(text) end)
+    end,
+    print_if_enabled = function(self, text)
+        if self:is_enabled() then
+            self:print(text)
+        end
+    end,
+}
+
 mod.promise_sprint = false
 mod.pressed_forward = false
 mod.interrupt_sprint = false
@@ -31,26 +61,6 @@ local INTERRUPTED_INPUT = {
     weapon_extra_release = true,
     weapon_reload        = true,
 }
-
-local debug = {
-    is_enabled = function(self)
-        return modding_tools and modding_tools:is_enabled() and mod:get("enable_debug_modding_tools")
-    end,
-    print = function(self, text)
-        pcall(function() modding_tools:console_print(text) end)
-    end,
-    print_if_enabled = function(self, text)
-        if self:is_enabled() then
-            self:print(text)
-        end
-    end,
-}
-
-mod.on_all_mods_loaded = function()
-    -- modding_tools:watch("pressed_forward", mod, "pressed_forward")
-    -- modding_tools:watch("interrupt_sprint", mod, "interrupt_sprint")
-    -- modding_tools:watch("character_state", mod, "character_state")
-end
 
 mod.on_game_state_changed = function(status, state_name)
     local input_settings = Managers.save:account_data().input_settings
@@ -108,7 +118,7 @@ local _input_hook = function(func, self, action_name)
     end
 
     if action_name == "sprinting" then
-        if pressed and not mod:get("enable_hold_to_sprint") then
+        if pressed and not mod.settings["enable_hold_to_sprint"] then
             setPromise("Pressed Sprint")
         end
         return out or isPromised()
@@ -144,7 +154,7 @@ local PlayerUnitVisualLoadout = require("scripts/extension_systems/visual_loadou
 mod:hook("PlayerCharacterStateSprinting", "_check_transition", function(func, self, ...)
     local out = func(self, ...)
     if out == "walking" then
-        if mod:get("enable_keep_sprint_after_weapon_actions") then
+        if mod.settings["enable_keep_sprint_after_weapon_actions"] then
             local weapon_template = PlayerUnitVisualLoadout.wielded_weapon_template(self._visual_loadout_extension, self._inventory_component)
             if check_weapon_want_to_stop(weapon_template.keywords) then
                 clearPromise("wants_to_stop")
