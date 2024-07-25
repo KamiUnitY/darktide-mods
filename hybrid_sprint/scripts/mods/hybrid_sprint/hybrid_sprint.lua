d34c589d8653ca30def3c6e8f16a81962607c7fc
KamiUnitY <kamiunity141@gmail.com>
2024-07-26 02:14:57 +0700

    Move some code

--- hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint.lua
+++ hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint.lua
@@ -3,6 +3,8 @@
 local mod = get_mod("hybrid_sprint")
 local modding_tools = get_mod("modding_tools")
 
+local PlayerUnitVisualLoadout = require("scripts/extension_systems/visual_loadout/utilities/player_unit_visual_loadout")
+
 mod.settings = {
     enable_hold_to_sprint                   = mod:get("enable_hold_to_sprint"),
     enable_keep_sprint_after_weapon_actions = mod:get("enable_keep_sprint_after_weapon_actions"),
@@ -64,20 +66,6 @@ local INTERRUPTED_INPUT = {
     weapon_reload        = true,
 }
 
-mod.on_game_state_changed = function(status, state_name)
-    local input_settings = Managers.save:account_data().input_settings
-    input_settings.hold_to_sprint = true
-end
-
-mod:hook_require("scripts/settings/options/input_settings", function(instance)
-    for i, setting in ipairs(instance.settings) do
-        if setting.id == "hold_to_sprint" then
-            table.remove(instance.settings, i)
-            break
-        end
-    end
-end)
-
 local function setPromise(from)
     if not mod.promise_sprint and ALLOWED_CHARACTER_STATE[mod.character_state] then
         mod.promise_sprint = true
@@ -99,6 +87,63 @@ local function isPromised()
     end
     return result
 end
+
+-- FORCE HOLD SPRINT IN VANILLA SETTINGS --
+
+mod.on_game_state_changed = function(status, state_name)
+    local input_settings = Managers.save:account_data().input_settings
+    input_settings.hold_to_sprint = true
+end
+
+-- REMOVE HOLD SPRINT FROM VANILLA SETTINGS --
+
+mod:hook_require("scripts/settings/options/input_settings", function(instance)
+    for i, setting in ipairs(instance.settings) do
+        if setting.id == "hold_to_sprint" then
+            table.remove(instance.settings, i)
+            break
+        end
+    end
+end)
+
+-- ON EVERY FRAME --
+
+local function check_weapon_want_to_stop(keywords)
+    local has = {}
+    for _, keyword in ipairs(keywords) do
+        has[keyword] = true
+    end
+    local melee = has["melee"]
+    local ranged = has["ranged"]
+    local want_to_stop = (melee and not has["combat_knife"]) or (ranged and has["heavystubber"])
+    return want_to_stop
+end
+
+mod:hook("PlayerCharacterStateSprinting", "_check_transition", function(func, self, ...)
+    local out = func(self, ...)
+    if out == "walking" then
+        if mod.settings["enable_keep_sprint_after_weapon_actions"] then
+            local weapon_template = PlayerUnitVisualLoadout.wielded_weapon_template(self._visual_loadout_extension, self._inventory_component)
+            if check_weapon_want_to_stop(weapon_template.keywords) then
+                clearPromise("wants_to_stop")
+            end
+        else
+            clearPromise("wants_to_stop")
+        end
+    end
+    return out
+end)
+
+mod:hook_safe("CharacterStateMachine", "fixed_update", function(self, unit, dt, t, frame, ...)
+    if self._unit_data_extension._player.viewport_name == 'player1' then
+        mod.character_state = self._state_current.name
+        if not ALLOWED_CHARACTER_STATE[mod.character_state] then
+            clearPromise("Unallowed Character State")
+        end
+    end
+end)
+
+-- INPUT HOOK --
 
 local _input_hook = function(func, self, action_name)
     local out = func(self, action_name)
@@ -131,51 +176,3 @@ end
 
 mod:hook("InputService", "_get", _input_hook)
 mod:hook("InputService", "_get_simulate", _input_hook)
-
-mod:hook_safe("CharacterStateMachine", "fixed_update", function(self, unit, dt, t, frame, ...)
-    if self._unit_data_extension._player.viewport_name == 'player1' then
-        mod.character_state = self._state_current.name
-        if not ALLOWED_CHARACTER_STATE[mod.character_state] then
-            clearPromise("Unallowed Character State")
-        end
-    end
-end)
-
-local function check_weapon_want_to_stop(keywords)
-    local has = {}
-    for _, keyword in ipairs(keywords) do
-        has[keyword] = true
-    end
-    local melee = has["melee"]
-    local ranged = has["ranged"]
-    local want_to_stop = (melee and not has["combat_knife"]) or (ranged and has["heavystubber"])
-    return want_to_stop
-end
-
-local PlayerUnitVisualLoadout = require("scripts/extension_systems/visual_loadout/utilities/player_unit_visual_loadout")
-mod:hook("PlayerCharacterStateSprinting", "_check_transition", function(func, self, ...)
-    local out = func(self, ...)
-    if out == "walking" then
-        if mod.settings["enable_keep_sprint_after_weapon_actions"] then
-            local weapon_template = PlayerUnitVisualLoadout.wielded_weapon_template(self._visual_loadout_extension, self._inventory_component)
-            if check_weapon_want_to_stop(weapon_template.keywords) then
-                clearPromise("wants_to_stop")
-            end
-        else
-            clearPromise("wants_to_stop")
-        end
-    end
-    return out
-end)
-
--- mod:hook_require("scripts/extension_systems/character_state_machine/character_states/utilities/sprint", function(instance)
---     mod:hook_safe(instance, "sprint_input", function(input_source, is_sprinting, sprint_requires_press_to_interrupt)
---         mod.interrupt_sprint = sprint_requires_press_to_interrupt
---     end)
--- end)
-
--- mod:hook_require("scripts/utilities/attack/interrupt", function(instance)
---     mod:hook_safe(instance, "action", function(t, unit, reason, reason_data_or_nil, ignore_immunity)
---         debug:print("Interrupt reason: " .. reason)
---     end)
--- end)
