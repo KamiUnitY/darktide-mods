60f31251216886127117d12733a78d40193ae133
KamiUnitY <kamiunity141@gmail.com>
2024-09-10 21:06:22 +0700

    Reorder language

--- guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action_localization.lua
+++ guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action_localization.lua
@@ -1,42 +1,42 @@
 return {
 	mod_name = {
 		en = "Guarantee Special Action",
-		["zh-cn"] = "保证特殊动作",
 		ru = "Гарантированная активация специального действия",
+		["zh-cn"] = "保证特殊动作",
 	},
 	mod_description = {
 		en = "Guarantee special action and reload action when the player try to perform them in the middle of certain animations",
+		ru = "Гарантированное выполнение специального действия и перезарядки, когда игрок пытается выполнить их во время определённых анимаций.",
 		["zh-cn"] = "确保玩家在动画中途能成功使用特殊动作和装弹动作",
-		ru = "Guarantee Special Action - Гарантированное выполнение специального действия и перезарядки, когда игрок пытается выполнить их во время определённых анимаций.",
 	},
 	enable_blocking_cancel_special = {
 		en = "Blocking Cancel Special Action",
-		["zh-cn"] = "格挡取消特殊动作",
 		ru = "Блокировка отменяет специальное действие",
+		["zh-cn"] = "格挡取消特殊动作",
 	},
 	enable_blocking_cancel_special_description = {
 		en = "While using melee weapon, Prioritize blocking over special action.",
-		["zh-cn"] = "使用近战武器时，优先格挡而非特殊动作。",
 		ru = "При использовании оружия ближнего боя отдавать приоритет блокированию, а не специальным действиям.",
+		["zh-cn"] = "使用近战武器时，优先格挡而非特殊动作。",
 	},
 	enable_ads_cancel_special = {
 		en = "ADS Cancel Special Action",
-		["zh-cn"] = "瞄准取消特殊动作",
 		ru = "Прицеливание отменяет специальное действие",
+		["zh-cn"] = "瞄准取消特殊动作",
 	},
 	enable_ads_cancel_special_description = {
 		en = "While using ranged weapon, Prioritize ADS over special action.",
-		["zh-cn"] = "使用远程武器时，优先瞄准而非特殊动作。",
 		ru = "При использовании оружия дальнего боя отдавать приоритет прицеливанию, а не специальным действиям.",
+		["zh-cn"] = "使用远程武器时，优先瞄准而非特殊动作。",
 	},
 	debug_group = {
 		en = "Debug",
-		["zh-cn"] = "调试",
 		ru = "Отладка",
+		["zh-cn"] = "调试",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
-		["zh-cn"] = "模组开发者工具",
 		ru = "Инструменты модификации",
+		["zh-cn"] = "模组开发者工具",
 	},
 }
