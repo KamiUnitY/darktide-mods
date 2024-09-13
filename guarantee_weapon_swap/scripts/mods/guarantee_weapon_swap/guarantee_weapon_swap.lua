c7970bd3a69e06876553ad72647b7c70337f9b96
KamiUnitY <kamiunity141@gmail.com>
2024-09-14 02:37:01 +0700

    Revised mod.promise_exist = false

--- guarantee_weapon_swap/scripts/mods/guarantee_weapon_swap/guarantee_weapon_swap.lua
+++ guarantee_weapon_swap/scripts/mods/guarantee_weapon_swap/guarantee_weapon_swap.lua
@@ -147,7 +147,7 @@ end
 local function clearPromise(action)
     if mod.promises[action] then
         mod.promises[action] = false
-        mod.promise_exist = false -- Every setPromise() got clearAllPromises() first, So this is fine
+        mod.promise_exist = table.contains(mod.promises, true)
     end
 end
 
