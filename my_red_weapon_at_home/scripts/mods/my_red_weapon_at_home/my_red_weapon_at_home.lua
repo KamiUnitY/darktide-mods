4d7207ae517c03b12de12b40be5371651cfe7e42
KamiUnitY <kamiunity141@gmail.com>
2024-10-13 18:11:06 +0700

    Revised item_stats_blueprints

--- my_red_weapon_at_home/scripts/mods/my_red_weapon_at_home/my_red_weapon_at_home.lua
+++ my_red_weapon_at_home/scripts/mods/my_red_weapon_at_home/my_red_weapon_at_home.lua
@@ -1,13 +1,6 @@
 local mod = get_mod("my_red_weapon_at_home")
 local modding_tools = get_mod("modding_tools")
 
-local Items = require("scripts/utilities/items")
-local UIFonts = require("scripts/managers/ui/ui_fonts")
-local UIFontSettings = require("scripts/managers/ui/ui_font_settings")
-local UIRenderer = require("scripts/managers/ui/ui_renderer")
-local UISettings = require("scripts/settings/ui/ui_settings")
-local WeaponTemplate = require("scripts/utilities/weapon/weapon_template")
-
 --------------------------
 -- MOD SETTINGS CACHING --
 --------------------------
@@ -109,77 +102,20 @@ mod:hook_safe("InventoryView", "_update_blueprint_widgets", function(self, ...)
 	end
 end)
 
-mod:hook(package.loaded, "scripts/ui/view_content_blueprints/item_stats_blueprints", function(generate_blueprints_function, grid_size, optional_item)
-    local blueprints = generate_blueprints_function(grid_size, optional_item)
-	if not blueprints.weapon_header or not blueprints.weapon_header.init then
-		return blueprints
-	end
+require("scripts/ui/view_content_blueprints/item_stats_blueprints")
+mod:hook(package.loaded, "scripts/ui/view_content_blueprints/item_stats_blueprints", function(func, ...)
+    local blueprints = func(...)
 
-    --#region FATSHARK CODE
-    local function _style_text_height(text, style, ui_renderer)
-        local text_font_data = UIFonts.data_by_type(style.font_type)
-        local text_font = text_font_data.path
-        local text_size = style.size
-        local use_max_extents = true
-        local text_options = UIFonts.get_font_options_by_style(style)
-        local _, text_height = UIRenderer.text_size(ui_renderer, text, style.font_type, style.font_size, text_size, text_options, use_max_extents)
-
-        return text_height
+    if not blueprints.weapon_header or not blueprints.weapon_header.init then
+        return blueprints
     end
 
-    local weapon_sub_display_name_style = table.clone(UIFontSettings.header_3)
-    local weapon_display_name_style = table.clone(UIFontSettings.header_3)
-    local weapon_rarity_name_style = table.clone(weapon_sub_display_name_style)
-    --#endregion
+    local _init = blueprints.weapon_header.init
 
     blueprints.weapon_header.init = function (parent, widget, element, callback_name, _, ui_renderer)
-        --#region FATSHARK CODE
-        local content = widget.content
-        local style = widget.style
-        content.element = element
+        _init(parent, widget, element, callback_name, _, ui_renderer)
 
         local item = element.item
-        local sub_display_name = Items.weapon_card_sub_display_name(item)
-        local rarity_name = Items.sub_display_name(item, nil, true)
-
-        content.sub_display_name = sub_display_name
-        content.rarity_name = rarity_name
-        content.icon = item.hud_icon or "content/ui/materials/icons/weapons/hud/combat_blade_01"
-
-        local rarity_name_style = style.rarity_name
-        local sub_display_name_text_height = _style_text_height(sub_display_name, weapon_display_name_style, ui_renderer)
-
-        rarity_name_style.offset[2] = rarity_name_style.offset[2] + sub_display_name_text_height
-
-        local weapon_template = WeaponTemplate.weapon_template_from_item(item)
-        local displayed_attacks = weapon_template.displayed_attacks
-        local weapon_special = displayed_attacks.special
-
-        if weapon_special then
-            content.weapon_special_icon = UISettings.weapon_action_type_icons[weapon_special.type]
-            content.weapon_special_text = Localize(weapon_special.display_name)
-            style.weapon_special_icon.visible = true
-            style.weapon_special_text.visible = true
-
-            local rarity_name_text_height = _style_text_height(rarity_name, weapon_rarity_name_style, ui_renderer)
-
-            style.weapon_special_icon.offset[2] = style.weapon_special_icon.offset[2] + sub_display_name_text_height + rarity_name_text_height
-            style.weapon_special_text.offset[2] = style.weapon_special_text.offset[2] + sub_display_name_text_height + rarity_name_text_height
-        else
-            style.weapon_special_icon.visible = false
-            style.weapon_special_text.visible = false
-        end
-
-        local rarity_color, rarity_color_dark = Items.rarity_color(item)
-
-        style.gradient_background.color = table.clone(rarity_color_dark)
-        style.gradient_background.material_values = {
-            invert = 1,
-        }
-        style.background.visible = not not element.add_background
-        style.rarity_name.text_color = table.clone(rarity_color)
-        --#endregion
-
         local rarity = item.__master_item.rarity
         local level = item.__master_item.baseItemLevel
 
