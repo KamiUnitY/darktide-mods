528b4eb997de1292ee5a745591cab42564737494
KamiUnitY <kamiunity141@gmail.com>
2024-10-13 22:13:31 +0700

    Revised red_weapons

--- red_weapons_at_home/scripts/mods/red_weapons_at_home/red_weapons_at_home.lua
+++ red_weapons_at_home/scripts/mods/red_weapons_at_home/red_weapons_at_home.lua
@@ -1,6 +1,13 @@
 local mod = get_mod("red_weapons_at_home")
 local modding_tools = get_mod("modding_tools")
 
+---------------
+-- CONSTANTS --
+---------------
+
+local COLOR = {255, 200, 25, 25}
+local COLOR_DARK = {255, 120, 20, 20}
+
 -------------------------
 -- MODDING TOOLS DEBUG --
 -------------------------
@@ -14,129 +21,114 @@ local debug = {
     end,
 }
 
+-------------------
+-- HELPER METHOD --
+-------------------
+
+local function set_color(target, color)
+    if target then
+        target[1], target[2], target[3], target[4] = color[1], color[2], color[3], color[4]
+    end
+end
+
+local function apply_sainted_theme(widget)
+    local rarity_tag = widget.style.rarity_tag and widget.style.rarity_tag.color
+    local rarity_name = widget.style.rarity_name and widget.style.rarity_name.text_color
+    local background_gradient = widget.style.background_gradient and widget.style.background_gradient.color
+    local gradient_background = widget.style.gradient_background and widget.style.gradient_background.color
+
+    set_color(rarity_tag, COLOR)
+    set_color(rarity_name, COLOR)
+    set_color(background_gradient, COLOR)
+    set_color(gradient_background, COLOR_DARK)
+
+    widget.content.rarity_name = Localize("loc_item_weapon_rarity_6")
+end
+
+
+local function is_sainted_item(item)
+    return item and item.__master_item.rarity == 5 and item.__master_item.baseItemLevel >= 380
+end
+
 ---------------
 -- ITEM GRID --
 ---------------
 
 mod:hook("ViewElementGrid", "_create_entry_widget_from_config", function(func, self, ...)
-	local widget, alignment_widget = func(self, ...)
+    local widget, alignment_widget = func(self, ...)
 
-	if not widget or (widget.type ~= "item" and widget.type ~= "store_item") then
-		return widget, alignment_widget
-	end
+    if widget and (widget.type == "item" or widget.type == "store_item") then
+        local item = widget.content and widget.content.element and widget.content.element.item
+        if is_sainted_item(item) then
+            apply_sainted_theme(widget)
+        end
+    end
 
-	local item = widget.content and widget.content.element and widget.content.element.item
-	if not item then
-		return widget, alignment_widget
-	end
-
-	local rarity = item.__master_item.rarity
-	local level = item.__master_item.baseItemLevel
-
-	if rarity == 5 and level >= 380 then
-		local bg_color = widget.style.background_gradient.color
-		local tag_color = widget.style.rarity_tag.color
-		local name_color = widget.style.rarity_name.text_color
-
-		bg_color[2], bg_color[3], bg_color[4] = 220, 20, 20
-		tag_color[2], tag_color[3], tag_color[4] = 220, 20, 20
-		name_color[2], name_color[3], name_color[4] = 220, 20, 20
-
-		widget.content.rarity_name = "Relic"
-	end
-
-	return widget, alignment_widget
+    return widget, alignment_widget
 end)
+
+------------------
+-- ITEM TOOLTIP --
+------------------
 
 require("scripts/ui/view_content_blueprints/item_stats_blueprints")
 mod:hook(package.loaded, "scripts/ui/view_content_blueprints/item_stats_blueprints", function(func, ...)
     local blueprints = func(...)
 
-    if not blueprints.weapon_header or not blueprints.weapon_header.init or not blueprints.extended_weapon_stats_header.init then
-        return blueprints
-    end
+    local function hook_weapon_header_init(blueprints)
+        local _weapon_header_init = blueprints.weapon_header.init
+        blueprints.weapon_header.init = function(parent, widget, element, callback_name, _, ui_renderer)
+            _weapon_header_init(parent, widget, element, callback_name, _, ui_renderer)
 
-    local _weapon_header_init = blueprints.weapon_header.init
-    blueprints.weapon_header.init = function (parent, widget, element, callback_name, _, ui_renderer)
-        _weapon_header_init(parent, widget, element, callback_name, _, ui_renderer)
-
-        local item = element.item
-        local rarity = item.__master_item.rarity
-        local level = item.__master_item.baseItemLevel
-
-        if rarity == 5 and level >= 380 then
-            local bg_color = widget.style.gradient_background.color
-            local name_color = widget.style.rarity_name.text_color
-
-            bg_color[2], bg_color[3], bg_color[4] = 130, 20, 20
-            name_color[2], name_color[3], name_color[4] = 220, 20, 20
-
-            widget.content.rarity_name = "Relic"
+            local item = element.item
+            if is_sainted_item(item) then
+                apply_sainted_theme(widget)
+            end
         end
     end
 
-    local _extended_weapon_stats_header_init = blueprints.extended_weapon_stats_header.init
-    blueprints.extended_weapon_stats_header.init = function (parent, widget, element, callback_name)
-        _extended_weapon_stats_header_init(parent, widget, element, callback_name)
+    local function hook_extended_weapon_stats_header_init(blueprints)
+        local _extended_weapon_stats_header_init = blueprints.extended_weapon_stats_header.init
+        blueprints.extended_weapon_stats_header.init = function(parent, widget, element, callback_name)
+            _extended_weapon_stats_header_init(parent, widget, element, callback_name)
 
-        local item = element.item
-        local rarity = item.__master_item.rarity
-        local level = item.__master_item.baseItemLevel
-
-        if rarity == 5 and level >= 380 then
-            local name_color = widget.style.rarity_name.text_color
-
-            name_color[2], name_color[3], name_color[4] = 220, 20, 20
-
-            widget.content.rarity_name = "Relic"
+            local item = element.item
+            if is_sainted_item(item) then
+                apply_sainted_theme(widget)
+            end
         end
+    end
+
+    if blueprints.weapon_header and blueprints.extended_weapon_stats_header then
+        hook_weapon_header_init(blueprints)
+        hook_extended_weapon_stats_header_init(blueprints)
     end
 
     return blueprints
 end)
 
-mod:hook_require("scripts/ui/views/inventory_view/inventory_view_content_blueprints", function(instance)
+---------------
+-- ITEM SLOT --
+---------------
 
+mod:hook_require("scripts/ui/views/inventory_view/inventory_view_content_blueprints", function(instance)
     local _item_slot_init = instance.item_slot.init
-    instance.item_slot.init = function (parent, widget, element, callback_name)
+    instance.item_slot.init = function(parent, widget, element, callback_name)
         _item_slot_init(parent, widget, element, callback_name)
 
-		local item = parent:equipped_item_in_slot(element.slot.name)
-        local rarity = item.__master_item.rarity
-        local level = item.__master_item.baseItemLevel
-
-        if rarity == 5 and level >= 380 then
-            local bg_color = widget.style.background_gradient.color
-            local tag_color = widget.style.rarity_tag.color
-            local name_color = widget.style.rarity_name.text_color
-
-            bg_color[2], bg_color[3], bg_color[4] = 220, 20, 20
-            tag_color[2], tag_color[3], tag_color[4] = 220, 20, 20
-            name_color[2], name_color[3], name_color[4] = 220, 20, 20
-
-            widget.content.rarity_name = "Relic"
+        local item = parent:equipped_item_in_slot(element.slot.name)
+        if is_sainted_item(item) then
+            apply_sainted_theme(widget)
         end
     end
 
     local _item_slot_update = instance.item_slot.update
-    instance.item_slot.update = function (parent, widget, input_service, dt, t, ui_renderer)
+    instance.item_slot.update = function(parent, widget, input_service, dt, t, ui_renderer)
         _item_slot_update(parent, widget, input_service, dt, t, ui_renderer)
 
-		local item = parent:equipped_item_in_slot(widget.content.element.slot.name)
-        local rarity = item.__master_item.rarity
-        local level = item.__master_item.baseItemLevel
-
-        if rarity == 5 and level >= 380 then
-            local bg_color = widget.style.background_gradient.color
-            local tag_color = widget.style.rarity_tag.color
-            local name_color = widget.style.rarity_name.text_color
-
-            bg_color[2], bg_color[3], bg_color[4] = 220, 20, 20
-            tag_color[2], tag_color[3], tag_color[4] = 220, 20, 20
-            name_color[2], name_color[3], name_color[4] = 220, 20, 20
-
-            widget.content.rarity_name = "Relic"
+        local item = parent:equipped_item_in_slot(widget.content.element.slot.name)
+        if is_sainted_item(item) then
+            apply_sainted_theme(widget)
         end
     end
-
 end)
