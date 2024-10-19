972b8799394350ff84864e2a186a0b72dd53d357
KamiUnitY <kamiunity141@gmail.com>
2024-10-19 07:59:07 +0700

    Simplify code

--- red_weapons_at_home/scripts/mods/red_weapons_at_home/red_weapons_at_home.lua
+++ red_weapons_at_home/scripts/mods/red_weapons_at_home/red_weapons_at_home.lua
@@ -92,6 +92,10 @@ local function get_trait_data(id, value)
 	return trait, output
 end
 
+------------------
+-- FIND SAINTED --
+------------------
+
 local function is_sainted_item(item)
     local expertise_level_text = Items.expertise_level(item, true)
     local expertise_level = type(expertise_level_text) == "string" and tonumber(expertise_level_text)
@@ -109,162 +113,26 @@ local function is_sainted_item(item)
     return false
 end
 
-local function apply_sainted_theme(widget, type, item)
-    local style = widget.style
+-------------------
+-- APPLY SAINTED --
+-------------------
 
-    if style.rarity_tag then
-        style.rarity_tag.color = table.clone(COLOR)
-    end
-    if style.background_gradient then
-        style.background_gradient.color = table.clone(COLOR)
-    end
-    if style.gradient_background then
-        style.gradient_background.color = table.clone(COLOR_DARK)
-    end
-
-    if item.item_type == "GADGET" then
-        if type == "TOOLTIP" then
-            if style.background then
-                style.background.color = table.clone(COLOR)
-            end
-            if style.display_name then
-                style.display_name.text_color = table.clone(COLOR)
-            end
-        end
-        if style.sub_display_name then
-            style.sub_display_name.text_color = table.clone(COLOR)
-        end
-        widget.content.sub_display_name = Localize("loc_item_weapon_rarity_6")
-    else
-        if style.rarity_name then
-            style.rarity_name.text_color = table.clone(COLOR)
-        end
-        widget.content.rarity_name = Localize("loc_item_weapon_rarity_6")
-    end
-end
-
----------------
--- ITEM GRID --
----------------
-
-mod:hook("ViewElementGrid", "_create_entry_widget_from_config", function(func, self, ...)
-    local widget, alignment_widget = func(self, ...)
-
-    if widget and (widget.type == "item" or widget.type == "store_item") then
-        local item = widget.content and widget.content.element and widget.content.element.item
+mod:hook_require("scripts/utilities/items", function(Items)
+    local _rarity_color = Items.rarity_color
+    Items.rarity_color = function(item)
+        local _color, _color_dark = _rarity_color(item)
         if is_sainted_item(item) then
-            apply_sainted_theme(widget, "GRID", item)
+            return COLOR, COLOR_DARK
         end
+        return _color, _color_dark
     end
 
-    return widget, alignment_widget
-end)
-
-------------------
--- ITEM TOOLTIP --
-------------------
-
-require("scripts/ui/view_content_blueprints/item_stats_blueprints")
-mod:hook(package.loaded, "scripts/ui/view_content_blueprints/item_stats_blueprints", function(func, ...)
-    local blueprints = func(...)
-
-    local _weapon_header_init = blueprints.weapon_header.init
-    blueprints.weapon_header.init = function(parent, widget, element, callback_name, _, ui_renderer)
-        _weapon_header_init(parent, widget, element, callback_name, _, ui_renderer)
-
-        local item = element.item
+    local _rarity_display_name = Items.rarity_display_name
+    Items.rarity_display_name = function(item)
+        local _name = _rarity_display_name(item)
         if is_sainted_item(item) then
-            apply_sainted_theme(widget, "TOOLTIP", item)
+            return Localize("loc_item_weapon_rarity_6")
         end
+        return _name
     end
-
-    local _extended_weapon_stats_header_init = blueprints.extended_weapon_stats_header.init
-    blueprints.extended_weapon_stats_header.init = function(parent, widget, element, callback_name)
-        _extended_weapon_stats_header_init(parent, widget, element, callback_name)
-
-        local item = element.item
-        if is_sainted_item(item) then
-            apply_sainted_theme(widget, "TOOLTIP", item)
-        end
-    end
-
-    local _gadget_header_init = blueprints.gadget_header.init
-    blueprints.gadget_header.init = function(parent, widget, element, callback_name, _, ui_renderer)
-        _gadget_header_init(parent, widget, element, callback_name, _, ui_renderer)
-
-        local item = element.item
-        if is_sainted_item(item) then
-            apply_sainted_theme(widget, "TOOLTIP", item)
-        end
-    end
-
-    return blueprints
-end)
-
----------------
--- ITEM SLOT --
----------------
-
-mod:hook_require("scripts/ui/views/inventory_view/inventory_view_content_blueprints", function(instance)
-    local _item_slot_init = instance.item_slot.init
-    instance.item_slot.init = function(parent, widget, element, callback_name)
-        _item_slot_init(parent, widget, element, callback_name)
-
-        local item = parent:equipped_item_in_slot(element.slot.name)
-        if is_sainted_item(item) then
-            apply_sainted_theme(widget, "SLOT", item)
-        end
-    end
-
-    local _item_slot_update = instance.item_slot.update
-    instance.item_slot.update = function(parent, widget, input_service, dt, t, ui_renderer)
-        _item_slot_update(parent, widget, input_service, dt, t, ui_renderer)
-
-        local item = parent:equipped_item_in_slot(widget.content.element.slot.name)
-        if is_sainted_item(item) then
-            apply_sainted_theme(widget, "SLOT", item)
-        end
-    end
-
-    local _gadget_item_slot_init = instance.gadget_item_slot.init
-    instance.gadget_item_slot.init = function(parent, widget, element, callback_name)
-        _gadget_item_slot_init(parent, widget, element, callback_name)
-
-        local item = parent:equipped_item_in_slot(element.slot.name)
-        if is_sainted_item(item) then
-            apply_sainted_theme(widget, "SLOT", item)
-        end
-    end
-
-    local _gadget_item_slot_update = instance.gadget_item_slot.update
-    instance.gadget_item_slot.update = function(parent, widget, input_service, dt, t, ui_renderer)
-        _gadget_item_slot_update(parent, widget, input_service, dt, t, ui_renderer)
-
-        local item = parent:equipped_item_in_slot(widget.content.element.slot.name)
-        if is_sainted_item(item) then
-            apply_sainted_theme(widget, "SLOT", item)
-        end
-    end
-end)
-
------------------------
--- ITEM NOTIFICATION --
------------------------
-
-mod:hook("ConstantElementNotificationFeed", "_generate_notification_data", function(func, self, message_type, data)
-    local notification = func(self, message_type, data)
-    if message_type == "item_granted" then
-        if is_sainted_item(notification.item) then
-            local rarity_color = table.clone(COLOR)
-            local background_rarity_color = table.clone(COLOR_DARK)
-			background_rarity_color[1] = background_rarity_color[1] * ConstantElementNotificationFeedSettings.default_alpha_value
-
-            notification.color = background_rarity_color
-            notification.line_color = rarity_color
-            notification.texts[1].color = rarity_color
-            notification.texts[2].color = rarity_color
-            notification.texts[2].display_name = Localize("loc_item_weapon_rarity_6")
-        end
-    end
-    return notification
 end)
