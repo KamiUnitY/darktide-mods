512dbde520312dbc1a5307a184595c0a912437ed
KamiUnitY <kamiunity141@gmail.com>
2024-12-03 20:35:59 +0700

    Updated version

--- guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action.lua
+++ guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action.lua
@@ -1,4 +1,4 @@
--- Guarantee Special Action by KamiUnitY. Ver. 1.1.2
+-- Guarantee Special Action by KamiUnitY. Ver. 1.1.3
 
 local mod = get_mod("guarantee_special_action")
 local modding_tools = get_mod("modding_tools")
