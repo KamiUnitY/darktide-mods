a8f4c0194236d431b54f7df97080fb8e87350a5d
SyuanTsai <carsun00@Gmail.com>
2025-06-28 13:15:46 +0800

    Add Guarantee Ability Activation

--- guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation_localization.lua
+++ guarantee_ability_activation/scripts/mods/guarantee_ability_activation/guarantee_ability_activation_localization.lua
@@ -6,40 +6,48 @@ return {
 		en = "Guarantee Ability Activation",
 		ru = "Гарантированная активация способности",
 		["zh-cn"] = "保证技能激活",
+		["zh-tw"] = "保證技能啟用",
 	},
 	mod_description = {
 		en = "Guarantee combat ability activation when player try to activate them in middle of certain animations",
 		ru = "Гарантированная активация боевой способности, когда игрок пытается активировать её в середине анимации",
 		["zh-cn"] = "确保玩家在动画中途能成功激活主动技能",
+		["zh-tw"] = "確保玩家在動畫中途能成功啟用主動技能",
 	},
 	enable_prevent_double_dashing = {
 		en = "Prevent double dashing",
 		ru = "Предотвращение двойного рывка",
 		["zh-cn"] = "防止二次冲锋",
+		["zh-tw"] = "防止二次衝鋒",
 	},
 	enable_prevent_double_dashing_description = {
 		en = "Blocking ability key press while dashing",
 		ru = "Блокировка нажатия клавиши способности во время рывка",
 		["zh-cn"] = "在冲锋过程中阻止技能键",
+		["zh-tw"] = "在衝鋒過程中阻止技能鍵",
 	},
 	enable_prevent_relic_cancel = {
 		en = "Prevent chorus cancel from ability key",
 		ru = "Предотвращение отмены Хора при нажатии клавиши способности",
 		["zh-cn"] = "防止按下技能键时取消圣咏",
+		["zh-tw"] = "防止按下技能鍵時取消禱告",
 	},
 	enable_prevent_ability_aiming = {
 		en = "Disable ability holding feature",
 		ru = "Отключение функции удержания способности",
 		["zh-cn"] = "禁用技能键按住功能",
+		["zh-tw"] = "禁用技能鍵按住功能",
 	},
 	debug_group = {
 		en = "Debug",
 		ru = "Отладка",
 		["zh-cn"] = "调试",
+		["zh-tw"] = "除錯",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
 		ru = "Инструменты модификации",
 		["zh-cn"] = "模组开发者工具",
+		["zh-tw"] = "模組開發者工具",
 	},
 }
