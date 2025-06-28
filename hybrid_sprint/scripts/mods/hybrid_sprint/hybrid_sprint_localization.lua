1cba1aeb6aa318b6c0b813f68919e574eb8072f4
SyuanTsai <carsun00@Gmail.com>
2025-06-28 13:18:38 +0800

    Add Hybrid Sprint

--- hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint_localization.lua
+++ hybrid_sprint/scripts/mods/hybrid_sprint/hybrid_sprint_localization.lua
@@ -6,65 +6,78 @@ return {
 		en = "Hybrid Sprint",
 		ru = "Гибридный бег",
 		["zh-cn"] = "混合式疾跑",
+		["zh-tw"] = "混合式衝刺",
 	},
 	mod_description = {
 		en = "Provide better sprinting experience by combining Toggle and Hold sprinting mechanic together. Sprint can be canceled by releasing forward key and pressing weapon action.",
 		ru = "Предлагает улучшенное управление бегом, объединяя механики бега «Переключить» и «Удерживать». Бег можно отменить, отпустив клавишу «Вперёд» и нажав любую кнопку действия оружия.",
 		["zh-cn"] = "结合切换疾跑和长按疾跑机制，提供体验更好的混合式疾跑。松开前进键和按下武器操作可以打断疾跑。",
+		["zh-tw"] = "透過結合「切換衝刺」與「長按衝刺」機制，提供更佳的衝刺體驗。鬆開前進鍵並按下武器操作即可取消衝刺。",
 	},
 	enable_hold_to_sprint = {
 		en = "Hold to Sprint",
 		ru = "Удерживать бег",
 		["zh-cn"] = "长按疾跑",
+		["zh-tw"] = "長按衝刺",
 	},
 	start_sprint_buffer = {
 		en = "Starting Sprint Buffer",
 		ru = "Буфер запуска спринта",
 		["zh-cn"] = "开始疾跑缓冲",
+		["zh-tw"] = "啟動衝刺緩衝",
 	},
 	start_sprint_buffer_description = {
 		en = "The duration in seconds for allowing sprint after pressing sprint, prior to pressing forward.",
 		ru = "Длительность в секундах, позволяющая спринт после нажатия кнопки спринта, перед нажатием вперед.",
 		["zh-cn"] = "按下疾跑键之后，按下前进键之前允许疾跑的持续时间（以秒为单位）。",
+		["zh-tw"] = "按下衝刺鍵後，在按下前進鍵之前允許衝刺的持續時間（以秒為單位）。"
 	},
 	enable_hold_to_sprint_description = {
 		en = "Only Sprint while holding the button, Basically disable this mod.",
 		ru = "Бежать только удерживая кнопку. Проще тогда отключить этот мод.",
 		["zh-cn"] = "仅在长按时疾跑，相当于禁用此模组。",
+		["zh-tw"] = "只有在長按按鍵時才會衝刺，基本上等同於停用此模組。",
 	},
 	enable_dodge_on_diagonal_sprint = {
 		en = "Dodge on Diagonal Sprint",
 		ru = "Уклонение при диагональном беге",
 		["zh-cn"] = "在斜跑时进行闪避",
+		["zh-tw"] = "斜向衝刺時閃避",
 	},
 	enable_dodge_on_diagonal_sprint_description = {
 		en = "Perform dodge instead of jump on diagonal sprint",
 		ru = "Выполняйте уклонение вместо прыжка при диагональном беге",
 		["zh-cn"] = "在斜跑时执行闪避而不是跳跃",
+		["zh-tw"] = "在斜向衝刺時進行閃避而非跳躍",
 	},
 	experimental_group = {
 		en = "Experimental",
 		ru = "Экспериментальное",
 		["zh-cn"] = "实验性",
+		["zh-tw"] = "實驗性",
 	},
 	enable_keep_sprint_after_weapon_action = {
 		en = "Keep Sprint after Weapon Action",
 		ru = "Продолжать бежать после использования оружия",
 		["zh-cn"] = "武器操作后继续疾跑",
+		["zh-tw"] = "武器操作後持續衝刺",
 	},
 	enable_keep_sprint_after_weapon_action_description = {
 		en = "Sprint will be continued after weapon action is finished, given that you still holding forward key.",
 		ru = "Бег будет продолжен после завершения действия оружия, будто вы всё ещё удерживаете клавишу «Вперёд».",
 		["zh-cn"] = "只要你仍然按住前进键，武器操作结束后就会继续疾跑。",
+		["zh-tw"] = "只要你仍然按住前進鍵，武器操作結束後即可繼續衝刺。",
 	},
 	debug_group = {
 		en = "Debug",
 		ru = "Отладка",
 		["zh-cn"] = "调试",
+		["zh-tw"] = "除錯",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
 		ru = "Инструменты модификации",
 		["zh-cn"] = "模组开发者工具",
+		["zh-tw"] = "模組開發者工具",
 	},
 }
