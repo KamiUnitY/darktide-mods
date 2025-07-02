93dda8b23ae8120c7d4dae08230112d8cba64216
SyuanTsai <carsun00@Gmail.com>
2025-07-02 18:46:50 +0800

    Update.

--- guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action_localization.lua
+++ guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action_localization.lua
@@ -3,40 +3,48 @@ return {
 		en = "Guarantee Special Action",
 		ru = "Гарантированная активация специального действия",
 		["zh-cn"] = "保证特殊动作",
+		["zh-tw"] = "保證特殊動作",
 	},
 	mod_description = {
 		en = "Guarantee special action and reload action when the player try to perform them in the middle of certain animations",
 		ru = "Гарантированное выполнение специального действия и перезарядки, когда игрок пытается выполнить их во время определённых анимаций.",
 		["zh-cn"] = "确保玩家在动画中途能成功使用特殊动作和装弹动作",
+		["zh-tw"] = "確保玩家在動畫中可以使用特殊動作和裝填",
 	},
 	enable_blocking_cancel_special = {
 		en = "Blocking Cancel Special Action",
 		ru = "Блокировка отменяет специальное действие",
 		["zh-cn"] = "格挡取消特殊动作",
+		["zh-tw"] = "格擋取消特殊動作",
 	},
 	enable_blocking_cancel_special_description = {
 		en = "While using melee weapon, Prioritize blocking over special action.",
 		ru = "При использовании оружия ближнего боя отдавать приоритет блокированию, а не специальным действиям.",
 		["zh-cn"] = "使用近战武器时，优先格挡而非特殊动作。",
+		["zh-tw"] = "使用近戰武器時，優先格擋而非特殊動作。",
 	},
 	enable_ads_cancel_special = {
 		en = "ADS Cancel Special Action",
 		ru = "Прицеливание отменяет специальное действие",
 		["zh-cn"] = "瞄准取消特殊动作",
+		["zh-tw"] = "瞄準取消特殊動作",
 	},
 	enable_ads_cancel_special_description = {
 		en = "While using ranged weapon, Prioritize ADS over special action.",
 		ru = "При использовании оружия дальнего боя отдавать приоритет прицеливанию, а не специальным действиям.",
 		["zh-cn"] = "使用远程武器时，优先瞄准而非特殊动作。",
+		["zh-tw"] = "使用遠程武器時，優先瞄準而非特殊動作。",
 	},
 	debug_group = {
 		en = "Debug",
 		ru = "Отладка",
 		["zh-cn"] = "调试",
+		["zh-tw"] = "除錯",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
 		ru = "Инструменты модификации",
 		["zh-cn"] = "模组开发者工具",
+		["zh-tw"] = "開發者模式",
 	},
 }
--- guarantee_weapon_swap/scripts/mods/guarantee_weapon_swap/guarantee_weapon_swap_localization.lua
+++ guarantee_weapon_swap/scripts/mods/guarantee_weapon_swap/guarantee_weapon_swap_localization.lua
@@ -6,30 +6,36 @@ return {
 		en = "Guarantee Weapon Swap",
 		ru = "Гарантированная смена оружия",
 		["zh-cn"] = "保证武器切换",
+		["zh-tw"] = "保證武器切換",
 	},
 	mod_description = {
 		en = "Guarantee weapon swapping when player try to swap weapon in middle of certain animations",
 		ru = "Гарантированная смена оружия, когда игрок пытается сменить оружие в середине анимации",
 		["zh-cn"] = "确保玩家在动画中途能成功切换武器",
+		["zh-tw"] = "確保玩家在動畫中能夠切換武器",
 	},
 	queue_limit = {
 		en = "Queue Limit",
 		ru = "Лимит очереди",
 		["zh-cn"] = "队列上限",
+		["zh-tw"] = "序列上限",
 	},
 	enable_zealot_throwing_knives = {
 		en = "Zealot's Blades of Faith",
 		ru = "«Клинки веры» изувера",
 		["zh-cn"] = "狂信徒的信仰之刃",
+		["zh-tw"] = "狂信徒的信仰之刃",
 	},
 	debug_group = {
 		en = "Debug",
 		ru = "Отладка",
 		["zh-cn"] = "调试",
+		["zh-tw"] = "除錯",
 	},
 	enable_debug_modding_tools = {
 		en = "Modding Tools",
 		ru = "Инструменты модификации",
 		["zh-cn"] = "模组开发者工具",
+		["zh-tw"] = "開發者模式",
 	},
 }
