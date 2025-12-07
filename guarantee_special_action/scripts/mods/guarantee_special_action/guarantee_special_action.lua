5bf2ffea5b8f30aea00f46555cc69dc4f34374d8
KamiUnitY <kamiunity141@gmail.com>
2025-12-07 20:59:37 +0700

    Added support for Hivescum weapons

--- guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action.lua
+++ guarantee_special_action/scripts/mods/guarantee_special_action/guarantee_special_action.lua
@@ -1,4 +1,4 @@
--- Guarantee Special Action by KamiUnitY. Ver. 1.1.8
+-- Guarantee Special Action by KamiUnitY. Ver. 1.1.9
 
 local mod = get_mod("guarantee_special_action")
 local modding_tools = get_mod("modding_tools")
